259e8b702c2a74ee12652e12cb01673ad8c1ed98
LunkRat <link@swanson.link>
2021-04-05 16:14:03 -0500

    CRD operations working via pydantic models. no update funciton yet.

--- proximatic/cli.py
+++ proximatic/cli.py
@@ -1,7 +1,5 @@
 import typer
 import pprint
-from typing import Optional
-from tabulate import tabulate
 from proximatic import Proximatic
 from .utils import tabulate_resources as tabulate_resources
 
@@ -11,19 +9,22 @@ proximatic = Proximatic()
 
 pp = pprint.PrettyPrinter(indent=4)
 
+
 @app.callback()
 def callback():
     """
     --------------\n
     Welcome to Proximatic CLI!\n
-    Interactive command-line interface for Proximatic.\n
+    Interactive command-line interface to Proximatic.\n
     --------------\n
     """
 
+
 @app.command()
-def config_show(path: str = '/data/traefik/conf/'):
+def config_show(path: str = "/data/traefik/conf/"):
     config = proximatic.system
-    typer.echo(pp.pprint(config.domains[0].routers[0].dict()))
+    typer.echo(pp.pprint(config.dict()))
+
 
 @app.command()
 def domain_list(id: str = None):
@@ -35,10 +36,36 @@ def domain_list(id: str = None):
     else:
         typer.echo(response.error)
 
+
 @app.command()
 def domain_export(id: str):
-    domain = proximatic.domain_fetch(id)
-    response = proximatic.domain_export(domain)
-    table = tabulate_resources(response)
-    typer.echo(f"Domain {id} successfully saved:")
-    typer.echo(table)
+    fetch = proximatic.domain_fetch(id)
+    if len(fetch) != 1:
+        typer.echo("No domins to export.")
+        typer.Abort()
+    else:
+        domain = fetch[0]
+        response = proximatic.domain_export(domain)
+        table = tabulate_resources(response)
+        typer.echo(f"Domain {id} successfully saved:")
+        typer.echo(table)
+
+
+@app.command()
+def domain_create(id: str, server: str):
+    response = proximatic.domain_create(id=id, server=server)
+    if response.data:
+        typer.echo(
+            f"\nSuccessfully created {response.data[0].type} {response.data[0].id}.\n"
+        )
+        table = tabulate_resources(response)
+        typer.echo(table)
+    else:
+        if response.error:
+            typer.echo(pp.pprint(response.error[0].dict()))
+
+
+@app.command()
+def domain_delete(id: str):
+    response = proximatic.domain_delete(id)
+    typer.echo(pp.pprint(response.dict()))
--- proximatic/core.py
+++ proximatic/core.py
@@ -1,19 +1,26 @@
 import os
 import yaml
 import requests
+from typing import List
 from pathlib import Path
-from jinja2 import Environment, PackageLoader
-from tabulate import tabulate
-from .models import (SystemConfigModel, DomainAttributesModel, ResponseModel,
-                    ResponseErrorModel, ResourceModel, RouterModel, ServiceModel,
-                    MiddleWaresModel, LoadBalancerModel, DomainExportModel)
+from .models import (
+    SystemConfigModel,
+    DomainAttributesModel,
+    ResponseModel,
+    ResponseErrorModel,
+    ResourceModel,
+    RouterModel,
+    ServiceModel,
+    LoadBalancerModel,
+    DomainExportModel,
+)
+
 
 class Proximatic:
     """The proximatic core engine."""
 
-    def __init__(
-        self, yml_path: str = '/data/traefik/conf', fqdn: str = None
-    ):
+    def __init__(self, yml_path: str = "/data/traefik/conf", fqdn: str = None):
+        """Bootstraps the Proximatic object with fqdn string and path to .yml files."""
         if yml_path:
             yml_path = Path(yml_path)
             if yml_path.exists() and yml_path.is_dir():
@@ -29,42 +36,50 @@ class Proximatic:
                 raise Exception()
 
     def load_config(self) -> SystemConfigModel:
-        files = self.system.yml_path.glob('**/*.yml')
+        """ETL function that reads all configuration in yml_path and loads resources into Proximatic()."""
+
+        files = self.system.yml_path.glob("**/*.yml")
         for filename in files:
             with open(filename, "r") as yml_stream:
+                # Load our yml file as dict.
                 config = yaml.safe_load(yml_stream)
-                if 'http' in config and 'services' in config['http']:
-                    router_id = list(config['http']['routers'].keys())[0]
-                    service_id = config['http']['routers'][router_id]['service']
-
+                # Extract the values into pydantic models.
+                if "http" in config and "services" in config["http"]:
+                    router_id = list(config["http"]["routers"].keys())[0]
+                    # We want the service that the router references.
+                    service_id = config["http"]["routers"][router_id]["service"]
+                    # Create our load balancer instance from our model.
                     loadbalancer = LoadBalancerModel(
-                            servers = config['http']['services'][service_id]['loadBalancer']['servers']
-                        )
-
-                    service = ServiceModel(
-                        id=service_id,
-                        loadBalancer=loadbalancer
-                        )
-
+                        servers=config["http"]["services"][service_id]["loadBalancer"][
+                            "servers"
+                        ]
+                    )
+                    # Create our service instance from our model and attach our loadbalancer.
+                    service = ServiceModel(id=service_id, loadBalancer=loadbalancer)
+                    # Create our router instance from our model.
                     router = RouterModel(
-                            id=router_id,
-                            entryPoints=config['http']['routers'][router_id]['entryPoints'],
-                            rule=config['http']['routers'][router_id]['rule'],
-                            middlewares=config['http']['routers'][router_id]['middlewares'],
-                            service=service_id
-                            )
-
+                        id=router_id,
+                        entryPoints=config["http"]["routers"][router_id]["entryPoints"],
+                        rule=config["http"]["routers"][router_id]["rule"],
+                        middlewares=config["http"]["routers"][router_id]["middlewares"],
+                        service=service_id,
+                    )
+                    # Create middleware instances from our model:
+                    middlewares = {}
+                    for name, configuration in config["http"]["middlewares"].items():
+                        middlewares[name] = configuration
+                    # Create our domain attributes instance from our model
+                    # and attach our data.
                     attributes = DomainAttributesModel(
                         router=router,
                         service=service,
+                        middlewares=middlewares,
                         endpoint=router.rule.split("`")[1],
-                        server=service.loadBalancer.servers[0]['url']
+                        server=service.loadBalancer.servers[0]["url"],
                     )
-                    
+
                     domain = ResourceModel(
-                        id=router_id,
-                        type="domain",
-                        attributes=attributes
+                        id=router_id, type="domain", attributes=attributes
                     )
 
                     self.system.domains.append(domain)
@@ -77,9 +92,9 @@ class Proximatic:
     def get_fqdn(self):
         return self.system.fqdn
 
-    def domain_list(self, id: str=None) -> ResponseModel:
+    def domain_list(self, id: str = None) -> ResponseModel:
         """
-        Returns a ResponseModel containing all Domain resources 
+        Returns a ResponseModel containing all Domain resources
         discovered in the active config.
         """
         response = ResponseModel()
@@ -96,24 +111,103 @@ class Proximatic:
             response.error = [ResponseErrorModel(id="changeme", detail=str(e))]
         return response
 
-    def domain_fetch(self, id: str) -> ResourceModel:
-        fetch = [domain for domain in self.system.domains if domain.id == id]
-        domain = fetch[0]
-        return domain
+    def domain_fetch(self, id: str) -> List[ResourceModel]:
+        self.load_config()
+        return next((item for item in self.system.domains if item.id == id), None)
 
     def domain_export(self, domain: ResourceModel) -> ResponseModel:
+        """File dump function that takes a domain resource model instance and writes it
+        to a proxy configuration .yml file.
+        """
+
         response = ResponseModel()
         if domain.type != "domain":
             error = ResponseErrorModel(
-                id="Invalid type.",
-                detail="We need a better error system."
+                id="Invalid type.", detail="We need a better error system."
             )
             response.error.append(error)
             return response
-        file_path = self.system.yml_path.joinpath(domain.id + ".yml")
         export = DomainExportModel()
-        export.http['routers'][domain.id] = domain.attributes.router
-        export.http['services'][domain.id] = domain.attributes.service
-        with open(file_path, "wt") as fh:
-            yaml.dump(export.dict(), fh)
+        export.http["routers"][domain.id] = domain.attributes.router.dict(
+            exclude={"id"}
+        )
+        export.http["services"][domain.id] = domain.attributes.service.dict()
+        export.http["middlewares"] = domain.attributes.middlewares
+        file_path = self.system.yml_path.joinpath(domain.id + ".yml")
+        with open(file_path, "wt") as yml_stream:
+            yaml.dump(export.dict(), yml_stream)
         return ResponseModel()
+
+    def domain_create(self, id: str, server: str) -> ResponseModel:
+        response = ResponseModel()
+        fetched_domain = self.domain_fetch(id)
+        if fetched_domain:
+            response.error = [
+                ResponseErrorModel(id="changeme", detail="Domain already exists.")
+            ]
+            return response
+        # Check the URL for validity by visiting it and
+        # expecting a 200 response code from its server.
+        try:
+            result = requests.get(server).status_code
+            if result != 200:
+                response.error = [
+                    ResponseErrorModel(
+                        id="changeme", detail="server URL not reachable."
+                    )
+                ]
+                return response
+        except Exception as e:
+            response.error = [ResponseErrorModel(id="changeme", detail=str(e))]
+            return response
+        endpoint = f"{id}.{self.system.fqdn}"
+        router = RouterModel(
+            id=id,
+            rule=f"Host(`{endpoint}`)",
+            service=id,
+        )
+        service = ServiceModel(
+            loadBalancer=LoadBalancerModel(servers=[{"url": server}])
+        )
+        # Set the default headers middleware for this domain.
+        middlewares = {
+            f"{id}-headers": {
+                "headers": {
+                    "frameDeny": True,
+                    "sslRedirect": True,
+                    "browserXssFilter": True,
+                    "contentTypeNosniff": True,
+                    "forceSTSHeader": True,
+                    "stsIncludeSubdomains": True,
+                    "stsPreload": True,
+                }
+            }
+        }
+        attributes = DomainAttributesModel(
+            router=router,
+            middlewares=middlewares,
+            service=service,
+            endpoint=endpoint,
+            server=server,
+        )
+        domain = ResourceModel(id=id, type="domain", attributes=attributes)
+        self.domain_export(domain)
+        self.load_config()
+        fetched_domain = self.domain_fetch(id)
+        if fetched_domain:
+            response.data = [domain]
+        else:
+            response.error = [
+                ResponseErrorModel(id="changeme", detail="Domain not created.")
+            ]
+        return response
+
+    def domain_delete(self, id: str) -> ResponseModel:
+        domain = self.domain_fetch(id)
+        if domain and domain.id == id:
+            files = self.system.yml_path.rglob(f"{domain.id}.yml")
+            for path in files:
+                path.unlink()
+            return ResponseModel(meta={"result": "deleted"})
+        else:
+            return ResponseModel(meta={"result": "not found"})
